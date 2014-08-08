62cb6879c1faaebd58c6e3bf9aafea8ae1d70381
root <root@ip-172-31-21-156.us-west-2.compute.internal>
2014-08-08 19:44:50 +0000

    right wp-config configuration

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_topart');
+define('DB_NAME', 'topartco_wp_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'topartco_wp');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin123');
+define('DB_PASSWORD', 'DullerTurnsFluidTribal73');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -31,7 +31,7 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', 'wp_');
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
