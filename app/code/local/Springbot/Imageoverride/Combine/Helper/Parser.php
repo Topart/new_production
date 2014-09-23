dea168d2f85825d729ed3a95d3a5ff605b39a149
Alejandro Marroni <alejandrom@onetree.com>
2014-09-23 17:18:13 -0300

    sprintBot images fix

---
+++ app/code/local/Springbot/Imageoverride/Combine/Helper/Parser.php
@@ -0,0 +1,41 @@
+<?php
+
+class Springbot_Imageoverride_Combine_Helper_Parser extends Springbot_Combine_Helper_Parser {
+
+	public function getImageUrl($product) {
+		if($product instanceof Mage_Catalog_Model_Product) {
+            $image_path = 'http://d3odr912zwpuhm.cloudfront.net/small_images/';
+			$skuImg = $this->cleanSkuImg(strtoupper($product->getSku()),'DG',$image_path);
+			$image_path .= $skuImg;
+			return $image_path;
+		}
+		else {
+			return null;
+		}
+	}
+
+    public function cleanSkuImg($sku,$endSku,$cpath=""){
+        $imgSku = '';
+        if ( substr( $sku, strlen( $sku ) - strlen( $endSku ) ) === $endSku )
+        {
+            $dg_suffix_index = strrpos($sku, $endSku);
+            $imgSku = substr_replace($sku, "", $endSku, 2) . ".jpg";
+        }
+        else
+        {
+            $imgSku = $sku . ".jpg";
+        }
+
+        $cpath .= $imgSku;
+
+        $imgSize = getimagesize($cpath);
+
+        if ($imgSize === false) {
+            $imgSku = $sku . ".jpg";
+        }
+
+        return $imgSku;
+    }
+}
+
+
